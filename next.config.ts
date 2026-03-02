e3aabcd68a69b3587c00652fea75989605fdfe28
jesusmartinvz <jesus_martim@hotmail.com>
2026-03-02 12:43:25 -0500

    fx

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
-/* import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-	
-};
-
-export default nextConfig;
- */
-
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	output: 'export',
-	trailingSlash: true,
-	images: { unoptimized: true },
-	productionBrowserSourceMaps: true,
+	
 };
 
 export default nextConfig;
